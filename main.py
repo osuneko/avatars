81df3dd9aa767e9d5eb0fd3ad7a186972e6c8e17
Matty <mc360masteryt@gmail.com>
2021-07-08 19:06:11 +0100

    finna kms

---
+++ main.py
@@ -0,0 +1,17 @@
+import os, random
+from quart import Quart, send_file
+
+app, defaultAvatars = Quart(__name__), ["-1", "-2", "-3", "-4", "-5", "-6", "-7", "-8"]
+
+@app.route("/")
+async def homepage():
+	return await send_file("avatars/{}.png".format(random.choice(defaultAvatars)))
+
+@app.route("/<int:uid>")
+async def avatarRequest(uid):
+	if os.path.exists(f"avatars/{uid}.png"):
+		return await send_file(f"avatars/{uid}.png")
+
+	return await send_file("avatars/{}.png".format(random.choice(defaultAvatars)))
+
+app.run()
